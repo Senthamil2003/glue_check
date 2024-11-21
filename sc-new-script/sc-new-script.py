7a0177b449fe841d0b2730acc82b6d5a2e420294
Senthamil2003 <77163996+Senthamil2003@users.noreply.github.com>
2024-11-21 21:25:54 +0530

    sentha 2nd commit

--- sc-new-script/sc-new-script.py
+++ sc-new-script/sc-new-script.py
@@ -21,6 +21,22 @@ datasource = glueContext.create_dynamic_frame.from_options(
         "recurse": True
     }
 )
+datasource = glueContext.create_dynamic_frame.from_options(
+    connection_type="s3",
+    format="csv",  # Change format if needed
+    connection_options={
+        "paths": [f"s3://{bucket}/{key}"],
+        "recurse": True
+    }
+)
+datasource = glueContext.create_dynamic_frame.from_options(
+    connection_type="s3",
+    format="csv",  # Change format if needed
+    connection_options={
+        "paths": [f"s3://{bucket}/{key}"],
+        "recurse": True
+    }
+)
 print("hello sentha from my branch")
 
 # Perform ETL and write back to S3 or another target
