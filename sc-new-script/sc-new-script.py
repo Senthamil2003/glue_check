d55b978747a66d2a8be3a8c5dd0b62f9f1025ff7
Senthamil2003 <77163996+Senthamil2003@users.noreply.github.com>
2024-11-21 18:49:03 +0530

    Synchronizing job sc-new-script from AWS Glue at 2024/11/21 13:19:03

---
+++ sc-new-script/sc-new-script.py
@@ -0,0 +1,26 @@
+import sys
+from awsglue.context import GlueContext
+from pyspark.context import SparkContext
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+
+# Get parameters passed by Lambda
+args = sys.argv
+bucket = args[args.index('--source_bucket') + 1]
+key = args[args.index('--source_key') + 1]
+
+print(f"Processing file s3://{bucket}/{key}")
+
+# Load the file and perform transformations
+datasource = glueContext.create_dynamic_frame.from_options(
+    connection_type="s3",
+    format="csv",  # Change format if needed
+    connection_options={
+        "paths": [f"s3://{bucket}/{key}"],
+        "recurse": True
+    }
+)
+
+# Perform ETL and write back to S3 or another target
+datasource.show()
